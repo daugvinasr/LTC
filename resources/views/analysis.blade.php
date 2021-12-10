9dad18e2a12d19f890ced7e0b2e5cafe41527ff3
ormsher <osmis100@gmail.com>
2021-12-10 14:00:36 +0200

    hope viskas

--- resources/views/analysis.blade.php
+++ resources/views/analysis.blade.php
@@ -1,37 +1,44 @@
 @extends('layouts.table')
 
 @section('tableNames')
-    <th class="px-4 py-3 text-center">Vizito ID</th>
-    <th class="px-4 py-3 text-center">Vizito data</th>
-    <th class="px-4 py-3 text-center">Vizito laikas</th>
-    <th class="px-4 py-3 text-center">Ar pacientas reikalauja tyrimu</th>
-    <th class="px-4 py-3 text-center">Daktaras</th>
-    <th class="px-4 py-3 text-center">Pacientas</th>
-    <th class="px-4 py-3 text-center">Tyrimo būsena</th>
-    <th class="px-4 py-3 text-center">Komentarai</th>
+    @if($analysisData->isEmpty())
 
+    @else
+        <th class="px-4 py-3 text-center">Vizito ID</th>
+        <th class="px-4 py-3 text-center">Vizito data</th>
+        <th class="px-4 py-3 text-center">Vizito laikas</th>
+        <th class="px-4 py-3 text-center">Ar pacientas reikalauja tyrimu</th>
+        <th class="px-4 py-3 text-center">Daktaras</th>
+        <th class="px-4 py-3 text-center">Pacientas</th>
+        <th class="px-4 py-3 text-center">Tyrimo būsena</th>
+        <th class="px-4 py-3 text-center">Komentarai</th>
+    @endif
 @endsection
 
 @section('tableData')
-    @foreach($analysisData as $data)
-        <tr class="text-gray-700">
-            <td class="px-4 py-3 text-ms font-semibold border text-center">{{ $data->id_visit}}</td>
-            <td class="px-4 py-3 text-ms font-semibold border text-center">{{ $data->visitDate }}</td>
-            <td class="px-4 py-3 text-ms font-semibold border text-center">{{ $data->visitTime }}:00</td>
-            <td class="px-4 py-3 text-ms font-semibold border text-center">{{ $data->analysisRequestedByPatient == '1' ? 'Taip' : 'Ne' }}</td>
-            <td class="px-4 py-3 text-ms font-semibold border text-center">{{ $data->doctorIdToText->firstLastName}}</td>
-            <td class="px-4 py-3 text-ms font-semibold border text-center">{{ $data->patientIdToText->firstLastName}}</td>
-            <td class="px-4 py-3 text-ms font-semibold border text-center">{{ ($data->analysisStatus == 'notStarted' ? 'Nepradėta' : ($data->analysisStatus == 'inLabaratory' ? 'Laboratorijoje' : 'Išsiųsta gydytojui')) }}
-                <br>
-                <br>
+    @if($analysisData->isEmpty())
+        <p class="text-center text-gray-700 font-bold px-2 text-xl">Tyrimų nėra</p>
+    @else
+        @foreach($analysisData as $data)
+            <tr class="text-gray-700">
+                <td class="px-4 py-3 text-ms font-semibold border text-center">{{ $data->id_visit}}</td>
+                <td class="px-4 py-3 text-ms font-semibold border text-center">{{ $data->visitDate }}</td>
+                <td class="px-4 py-3 text-ms font-semibold border text-center">{{ $data->visitTime }}:00</td>
+                <td class="px-4 py-3 text-ms font-semibold border text-center">{{ $data->analysisRequestedByPatient == '1' ? 'Taip' : 'Ne' }}</td>
+                <td class="px-4 py-3 text-ms font-semibold border text-center">{{ $data->doctorIdToText->firstLastName}}</td>
+                <td class="px-4 py-3 text-ms font-semibold border text-center">{{ $data->patientIdToText->firstLastName}}</td>
+                <td class="px-4 py-3 text-ms font-semibold border text-center">{{ ($data->analysisStatus == 'notStarted' ? 'Nepradėta' : ($data->analysisStatus == 'inLabaratory' ? 'Laboratorijoje' : 'Išsiųsta gydytojui')) }}
+                    <br>
+                    <br>
 
-                <a href="/analysis/{{$data->id_visit}}" class="bg-blue-300 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">➜</a>
-            </td>
-            <td class="px-4 py-3 text-ms font-semibold border text-center">
-                <a href="/comments/{{$data->id_visit}}/{{$data->patientIdToText->id_user}}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                    {{$commentCount = \App\Models\comments::select('*')->where([['fk_visit','=',$data->id_visit]])->count()}}
-                </a>
-            </td>
-        </tr>
-    @endforeach
+                    <a href="/analysis/{{$data->id_visit}}" class="bg-blue-300 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">➜</a>
+                </td>
+                <td class="px-4 py-3 text-ms font-semibold border text-center">
+                    <a href="/comments/{{$data->id_visit}}/{{$data->patientIdToText->id_user}}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                        {{$commentCount = \App\Models\comments::select('*')->where([['fk_visit','=',$data->id_visit]])->count()}}
+                    </a>
+                </td>
+            </tr>
+        @endforeach
+    @endif
 @endsection
--- resources/views/visitsDoctor.blade.php
+++ resources/views/visitsDoctor.blade.php
@@ -1,33 +1,41 @@
 @extends('layouts.table')
 
 @section('tableNames')
-    <th class="px-4 py-3 text-center">Vizito ID</th>
-    <th class="px-4 py-3 text-center">Registracijos data</th>
-    <th class="px-4 py-3 text-center">Vizito data</th>
-    <th class="px-4 py-3 text-center">Laikas</th>
-    <th class="px-4 py-3 text-center">Ar pacientas reikalauja tyrimu</th>
-    <th class="px-4 py-3 text-center">Tyrimo būsena</th>
-    <th class="px-4 py-3 text-center">Pacientas</th>
-    <th class="px-4 py-3 text-center">Komentarai</th>
+    @if($visitsData->isEmpty())
+
+    @else
+        <th class="px-4 py-3 text-center">Vizito ID</th>
+        <th class="px-4 py-3 text-center">Registracijos data</th>
+        <th class="px-4 py-3 text-center">Vizito data</th>
+        <th class="px-4 py-3 text-center">Laikas</th>
+        <th class="px-4 py-3 text-center">Ar pacientas reikalauja tyrimu</th>
+        <th class="px-4 py-3 text-center">Tyrimo būsena</th>
+        <th class="px-4 py-3 text-center">Pacientas</th>
+        <th class="px-4 py-3 text-center">Komentarai</th>
+    @endif
 @endsection
 
 @section('tableData')
-    @foreach($visitsData as $data)
-        <tr class="text-gray-700">
-            <td class="px-4 py-3 text-ms font-semibold border text-center">{{ $data->id_visit}}</td>
-            <td class="px-4 py-3 text-ms font-semibold border text-center">{{ $data->registrationDate }}</td>
-            <td class="px-4 py-3 text-ms font-semibold border text-center">{{ $data->visitDate }}</td>
-            <td class="px-4 py-3 text-ms font-semibold border text-center">{{ $data->visitTime}}:00</td>
-            <td class="px-4 py-3 text-ms font-semibold border text-center">{{ $data->analysisRequestedByPatient == '1' ? 'Taip' : 'Ne' }}</td>
-            <td class="px-4 py-3 text-ms font-semibold border text-center">{{ ($data->analysisStatus == 'notStarted' ? 'Nepradėta' : ($data->analysisStatus == 'inLabaratory' ? 'Laboratorijoje' : 'Išsiųsta gydytojui')) }}</td>
-            <td class="px-4 py-3 text-ms font-semibold border text-center">{{ $data->patientIdToText->firstLastName}}</td>
-            <td class="px-4 py-3 text-ms font-semibold border text-center">
-                <a href="/comments/{{$data->id_visit}}/{{$data->patientIdToText->id_user}}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                    {{$commentCount = \App\Models\comments::select('*')->where([['fk_visit','=',$data->id_visit]])->count()}}
-                </a>
-            </td>
-        </tr>
-    @endforeach
+    @if($visitsData->isEmpty())
+        <p class="text-center text-gray-700 font-bold px-2 text-xl">Nėra vizitų</p>
+    @else
+        @foreach($visitsData as $data)
+            <tr class="text-gray-700">
+                <td class="px-4 py-3 text-ms font-semibold border text-center">{{ $data->id_visit}}</td>
+                <td class="px-4 py-3 text-ms font-semibold border text-center">{{ $data->registrationDate }}</td>
+                <td class="px-4 py-3 text-ms font-semibold border text-center">{{ $data->visitDate }}</td>
+                <td class="px-4 py-3 text-ms font-semibold border text-center">{{ $data->visitTime}}:00</td>
+                <td class="px-4 py-3 text-ms font-semibold border text-center">{{ $data->analysisRequestedByPatient == '1' ? 'Taip' : 'Ne' }}</td>
+                <td class="px-4 py-3 text-ms font-semibold border text-center">{{ ($data->analysisStatus == 'notStarted' ? 'Nepradėta' : ($data->analysisStatus == 'inLabaratory' ? 'Laboratorijoje' : 'Išsiųsta gydytojui')) }}</td>
+                <td class="px-4 py-3 text-ms font-semibold border text-center">{{ $data->patientIdToText->firstLastName}}</td>
+                <td class="px-4 py-3 text-ms font-semibold border text-center">
+                    <a href="/comments/{{$data->id_visit}}/{{$data->patientIdToText->id_user}}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                        {{$commentCount = \App\Models\comments::select('*')->where([['fk_visit','=',$data->id_visit]])->count()}}
+                    </a>
+                </td>
+            </tr>
+        @endforeach
+    @endif
 @endsection
 
 
